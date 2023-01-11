9c1b73fc499261bdd98f60fae25d72f31e57f400
Cocheci Ovidiu <cocheciovidiu30@gmail.comgit log>
2023-01-12 00:08:27 +0200

    initial commit

---
+++ operatori.js
@@ -0,0 +1,23 @@
+const rezultat = 2 + 3 * 4 - 5;
+console.log("Rezultat: ", rezultat)
+
+const rezultat2 = 4 * (2 + 6 / 3);
+console.log("Rezultat2: ", rezultat2);
+
+const rezultat3 = 2 * 3**3 + 2**5
+console.log("Rezultat3: ", rezultat3);
+
+const rezultat4 = (5 * (3 + 2/2) + 3);
+console.log("Rezultat4: ", rezultat4);
+
+const rezultat5 = ( 2**5 - 5**2 + 3 * (5 - 4/2) ) /  (2**5 + 5**2);
+console.log("Rezultat5: ", rezultat5)
+
+let x = 5
+let y = 8
+
+const rezultat6 = 2 * x - 5 * y
+console.log("Rezultat6: ", rezultat6);
+
+const rezultat7 = 3 * x + 9 * y;
+console.log("Rezultat7: ", rezultat7);
